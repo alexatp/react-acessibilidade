5c5ec065a1772d1de0b699827c148464b160cfa4
Alexandre Teixeira <alextp@gmail.com>
2025-01-31 18:06:12 -0300

    construção do componen accordion

---
+++ src/componentes/Acordeao/AcordeaoConteudo.tsx
@@ -0,0 +1,13 @@
+interface AcordeaoConteudoProps {
+    children: React.ReactNode;
+  }
+
+const AcordeaoConteudo = ({ children }:AcordeaoConteudoProps) => {
+  return (
+<details className="acordeao__conteudo">
+{children }
+</details>
+  )
+}
+
+export default AcordeaoConteudo
