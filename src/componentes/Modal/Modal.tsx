d926531279c4c36b97810903178c6b88e562d072
Alexandre Teixeira <alextp@gmail.com>
2025-02-02 01:34:12 -0300

    Melhorias na modal

--- src/componentes/Modal/Modal.tsx
+++ src/componentes/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useRef } from "react";
 import "./Modal.css";
 import ModalCabecalho from "./ModalCabecalho";
 import ModalConteudo from "./ModalConteudo";
@@ -8,7 +9,37 @@ interface ModalProps extends React.HTMLProps<HTMLDialogElement> {
     fecharModal: () => void;
 }
 
-const Modal = ({fecharModal, estaAberta, ariaLabel, ...rest }: ModalProps) => {
+const Modal = ({ fecharModal, estaAberta, ariaLabel, ...rest }: ModalProps) => {
+    const modalRef = useRef<HTMLDialogElement>(null);
+
+    const escutadorTecla = useCallback((evento: KeyboardEvent) => {
+        if (evento.key === "Escape") {
+            fecharModal();
+        }
+    },
+        [fecharModal]
+    );
+
+    const capturarFoco = useCallback((evento: FocusEvent) => {
+        if (!modalRef.current?.contains(evento.target as Node)) {
+            modalRef.current?.focus();
+        }
+    }, [])
+
+    useEffect(() => {
+        if (estaAberta) {
+            document.addEventListener("keydown", escutadorTecla);
+            document.addEventListener("focusin", capturarFoco);
+            
+            modalRef.current?.focus()
+        }
+        
+        return () => {
+            document.removeEventListener("keydown", escutadorTecla);
+            document.removeEventListener("focusin", capturarFoco);
+        }
+    }, [estaAberta, capturarFoco, escutadorTecla]);
+
     return (
         <>
             <div className="modal__overlay" onClick={fecharModal} />
@@ -18,9 +49,10 @@ const Modal = ({fecharModal, estaAberta, ariaLabel, ...rest }: ModalProps) => {
                 open={estaAberta}
                 onClose={fecharModal}
                 {...rest}
+                ref={modalRef}
             >
                 <ModalCabecalho aoFechar={fecharModal} />
-                <ModalConteudo aoFechar={fecharModal}/>
+                <ModalConteudo aoFechar={fecharModal} />
             </dialog>
         </>
     )
